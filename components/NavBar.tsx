b3c94b1078ecd5f32600b985b3a82b328319e2ff
Patrick Hultquist <patrick@patrick.today>
2021-11-11 15:06:21 -0500

    NavBar Improvements, Hero Image

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,10 +5,10 @@ import { SignUp } from "./";
 export default function NavBar(): FunctionComponent {
   return (
     <div className="fixed bottom-4 px-4 w-full z-50">
-      <div className="rounded-xl bg-white flex flex-row justify-between w-full overflow-hidden p-2 shadow-lg">
+      <div className="rounded-xl bg-white grid grid-cols-3 w-full overflow-hidden p-2 shadow-lg">
         <SignUp />
-        <div>Logo</div>
-        <div>Sign In</div>
+        <div className="my-auto text-center">Logo</div>
+        <div className="my-auto text-right">Sign In</div>
       </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,28 +2,37 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import { Tag, NavBar, Badge } from "../components";
+import Laptop from "../public/Laptop.png";
 
 const Home: NextPage = () => {
   return (
     <>
       <Head>
+      <meta name="theme-color" content="black" />
         <title>PicLab - THETAGLINE</title>
       </Head>
       <div className="bg-gradient-to-br from-gradient-home-start to-gradient-home-end">
         <NavBar />
-        <div className="p-2 h-screen">
+        <div className="h-screen flex flex-row">
           <div className="w-1/3 ml-10 pb-28 pt-10 h-full flex flex-col justify-between">
-            <p className="">Piclab</p>
+            <p className="py-10">Piclab</p>
             <div>
               <Badge text="Coming Summer 2021" />
               <h1 className="text-3xl text-white my-4">
                 Powerful image asset management for everyone
               </h1>
             </div>
-            <div className="border-l-2 border-white px-10 text-white">
+            <div className="border-l-2 border-white pl-10 pr-20 text-white">
               Organize, transform, & serve your entire photo library. The
               perfect image management app for freelancers, organizations, & web
               developers.
+            </div>
+          </div>
+          <div className="w-2/3 h-full overflow-hidden bg-red-100 bg-opacity-20">
+            {/* <Image src={Laptop} alt="PicLab" className="object-none object-bottom"/> */}
+            <div className="absolute bottom-0">
+              {/* <img src="/Laptop.png" alt="Piclab" className="" /> */}
+              <Image src={Laptop} alt="PicLab" className="absolute bottom-0" />
             </div>
           </div>
         </div>
