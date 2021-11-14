5b200166681ed19bc31cedfbb2963f4bbaff01df
Patrick Hultquist <patrick@patrick.today>
2021-11-13 19:53:55 -0500

    updated navbar

---
+++ components/EmailForm.tsx
@@ -0,0 +1,26 @@
+import { FunctionComponent } from "react";
+import Link from "next/link";
+
+interface EmailFormProps {
+  border?: boolean;
+}
+
+const EmailForm: FunctionComponent<EmailFormProps> = props => {
+  return (
+    <form action="">
+      <div className={`${props.border ? "border-2 border-gray-300" : "border-0"} flex flex-row p-2 rounded-lg overflow-hidden shadow-sm divide-x bg-white w-full`}>
+        <div className="w-full flex items-center">
+          <i aria-hidden className="far fa-envelope text-black opacity-50 pl-3 mr-2 pt-1" />
+          <input placeholder="Enter email" className="p-2 outline-none w-28 flex-grow" />
+        </div>
+        <div className="mx-auto pl-2">
+          <button type="submit" className="text-primary inline-block font-bold p-2 text-center w-full cursor-pointer whitespace-nowrap">
+            <p className="">Request Beta</p>
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default EmailForm
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,15 +1,28 @@
 import { FunctionComponent } from "react";
 import Link from "next/link";
-import { SignUp } from "./";
+import { EmailForm, Logo } from "./";
 
-export default function NavBar(): FunctionComponent {
+const NavBar: FunctionComponent = () => {
   return (
     <div className="fixed bottom-4 px-4 w-full z-50">
-      <div className="rounded-xl bg-white grid grid-cols-3 w-full overflow-hidden p-2 shadow-lg">
-        <SignUp />
-        <div className="my-auto text-center">Logo</div>
-        <div className="my-auto text-right">Sign In</div>
+      <div className="rounded-xl bg-white flex flex-row justify-between w-full overflow-hidden p-2 shadow-lg">
+        <div className="hidden w-1/3 md:block">
+          <EmailForm border />
+        </div>
+        <div className="my-auto text-center w-1/3">
+          <div className="w-28 md:mx-auto">
+            <Logo />
+          </div>
+        </div>
+        <div className="my-auto text-right w-1/3">
+          <button className="text-primary font-bold hover:bg-gray-100 rounded-md py-2 px-4 mr-2 transition-all">
+            <i aria-hidden className="fas fa-sign-in fa-lg mr-2" />
+            Sign In
+          </button>
+        </div>
       </div>
     </div>
   );
 }
+
+export default NavBar;
