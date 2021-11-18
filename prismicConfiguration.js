db64d23f701f1a01b30b0ace91977928f92fb00d
Patrick Hultquist <patrick@patrick.today>
2021-11-18 17:30:45 -0500

    use prismic cdn

--- prismicConfiguration.js
+++ prismicConfiguration.js
@@ -4,7 +4,7 @@ export const repoName = 'piclab-site'
 // -- Prismic API endpoint
 // Determines which repository to query and fetch data from
 // Configure your site's access point here
-export const apiEndpoint = `https://${repoName}.prismic.io/api/v2`
+export const apiEndpoint = `https://${repoName}.cdn.prismic.io/api/v2`
 
 // -- Access Token if the repository is not public
 // Generate a token in your dashboard and configure it here if your repository is private
